edab0a1e4b4827dedbe5a8f639b38494e8ed86fd
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-08 16:09:16 -0600

    Modal

--- src/Level/components/layout/Home.jsx
+++ src/Level/components/layout/Home.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
 import Background from "../../assets/img/Background.webp";
 import { ClerkProvider } from "@clerk/clerk-react";
+import Modal from "../ui/Modal";
 
 const Home = () => {
   return (
     <div className="relative w-full min-h-screen">
- 
       <div
         className="absolute inset-0 bg-fixed bg-center bg-cover"
         style={{ backgroundImage: `url(${Background})` }}
       ></div>
-
 
       <div className="absolute inset-0 bg-black bg-opacity-60"></div>
 
@@ -20,18 +24,16 @@ const Home = () => {
           Bienvenido a <span className="text-yellow-500">CódiGol</span>
         </h1>
         <p className="max-w-xl mb-8 text-lg md:text-xl">
-          Aprende <span className="text-yellow-400">Tailwind CSS</span> jugando y dominando estilos como un pro.
+          Aprende <span className="font-bold text-yellow-400">Tailwind CSS</span> jugando
+          y dominando estilos como un pro.
         </p>
 
- 
         <div className="flex flex-col w-full max-w-xs gap-4">
           <button className="px-6 py-3 font-bold text-black transition-all bg-yellow-500 hover:bg-yellow-600 rounded-xl">
             ¡Jugar Ahora!
           </button>
-          <button className="px-6 py-2 font-semibold text-black bg-white hover:bg-gray-200 rounded-xl">
-            Instrucciones
-          </button>
-          
+          <Modal />
+
           <SignedOut>
             <SignInButton mode="modal">
               <button className="px-6 py-2 text-white transition bg-black border border-white rounded-xl hover:bg-white hover:text-black">
@@ -45,7 +47,15 @@ const Home = () => {
         </div>
 
         <p className="mt-10 text-sm opacity-70">
-          Proyecto creado para la <span className="underline">Hackathon de MiduDev 2025</span>
+          Proyecto creado para la{" "}
+          <a
+          className="font-bold transition duration-300 ease-in-out hover:text-yellow-500"
+            href="https://github.com/midudev/hackaton-clerk-2025?tab=readme-ov-file#%EF%B8%8F-c%C3%B3mo-participar-en-la-hackat%C3%B3n"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hackathon de MiduDev 2025
+          </a>
         </p>
       </div>
     </div>
