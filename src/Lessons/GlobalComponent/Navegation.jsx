87eecb12f8414cb47a0a7e139622a886e5c94e92
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-09 10:47:35 -0600

    Adjustment Navigation Color

--- src/Lessons/GlobalComponent/Navegation.jsx
+++ src/Lessons/GlobalComponent/Navegation.jsx
@@ -33,7 +33,7 @@ const Navegation = () => {
       {/*Menu sizes less than 720 px */}
       {isMobile && (
         <div
-          className={`fixed top-0 left-0 w-full h-full bg-black/70 backdrop-blur-[4px] transition-transform duration-500 ease-in-out transform z-50 
+          className={`fixed text-white top-0 left-0 w-full h-full bg-black/70 backdrop-blur-[4px] transition-transform duration-500 ease-in-out transform z-50 
             ${menuOpen ? "translate-x-0" : "-translate-x-full"}`}
         >
           {/* Close menu */}
@@ -41,7 +41,7 @@ const Navegation = () => {
             className="absolute right-0 w-20 h-auto cursor-pointer top-5"
             onClick={toggleMenu}
           />
-          <nav className="p-4 text-white">
+          <nav className="p-4 ">
             <div className="flex flex-col justify-between gap-2 p-8">
               <h1 className="mb-4 text-5xl font-bold md:text-6xl">CódiGol</h1>
 
