f0ef9c7fe239d38ebd37ecf233ce553076c84b35
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-09 14:56:43 -0600

    FinalScorePage

--- src/screens/FinalScore.jsx
+++ src/screens/FinalScore.jsx
@@ -2,7 +2,8 @@
 import React, { useRef } from "react";
 import Background from "../assets/img/Background.webp";
 import Navigation from "../ui/Navigation";
-import DialogueBox from "../ui/DialogueBox";
+import WindJaguars from "../assets/img/WindJaguars.webp";
+import GemRubies from "../assets/img/GemRubies.webp";
 
 const FinalScore = () => {
   return (
@@ -17,12 +18,43 @@ const FinalScore = () => {
           <Navigation />
         </div>
 
-        <div className="relative grid min-h-screen grid-cols-1 col-span-4 ">
-          <h1 className="text-5xl font-bold text-black md:text-6xl drop-shadow-lg">
-            Lección 6: FinalScore
+        <div className="relative flex flex-col min-h-screen col-span-4 text-center text-white ">
+          <h1 className="text-6xl font-bold md:text-6xl drop-shadow-lg">
+            Liga Full Stack
           </h1>
 
-          <DialogueBox />
+          <h1 className="mt-4 text-5xl font-bold md:text-6xl drop-shadow-lg">
+            30:00
+          </h1>
+
+          <div className="grid grid-cols-3 m-auto max-w-[60rem]">
+            <div className="flex flex-col items-center justify-center gap-2 text-white">
+              <img
+                src={WindJaguars}
+                alt="WindJaguars"
+                className="w-full h-auto"
+              />
+              <div className="w-full h-2 bg-white"></div>
+              <h1 className="text-4xl font-bold text-center">Wind Jaguars</h1>
+            </div>
+
+            <div className="flex flex-col items-center justify-center gap-2 text-white">
+              <h1 className="font-bold text-center text-7xl">04 - 03</h1>
+            </div>
+
+            <div className="flex flex-col items-center justify-center gap-2 text-white">
+              <img src={GemRubies} alt="GemRubies" className="w-full h-auto" />
+              <div className="w-full h-2 bg-white"></div>
+              <h1 className="text-4xl font-bold text-center">Gem Rubies</h1>
+            </div>
+
+            <button
+              //onClick={handleNext}
+              className="col-start-3 px-4 py-2 m-4 font-semibold text-white bg-blue-700 rounded hover:bg-blue-800"
+            >
+              Siguiente
+            </button>
+          </div>
         </div>
       </div>
     </div>
