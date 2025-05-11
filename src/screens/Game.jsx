fad0270bb926dcfa4891b8a663f0ccad772694a9
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-11 15:46:42 -0600

    Game

--- src/screens/Game.jsx
+++ src/screens/Game.jsx
@@ -1,25 +1,72 @@
-//Interactive match stage
+//Practical training
+//Dialogues and exercise
 import React, { useRef } from "react";
 import { useEffect, useState } from "react";
 import TestFund from "../assets/img/TestFund.webp";
-import RivalGoalkeeper from "../assets/img/RivalGoalkeeper.webp";
 import Navigation from "../ui/Navigation";
 import DialogueBox from "../ui/DialogueBox";
 import TrainingPoint from "../ui/TrainingPoint";
+import RivalGoalkeeper from "../assets/img/RivalGoalkeeper.webp";
 import Clock from "../ui/Clock";
 import Preview from "../ui/Preview";
 import Editor from "../ui/Editor";
+//exercises and evaluate Answer
+import exercises from "../data/exercises";
+import evaluateAnswer from "../utils/evaluateAnswer";
+
+//At the end of the exercises it will take you to MatchPresentation
+import { useNavigate } from "react-router-dom";
+import { navigateToNextPhase } from "../utils/navigateToNextPhase";
 
 const defaultCode = `
-<div class="p-4 bg-blue-500 text-white rounded-lg">
-  Editor de código con Tailwind!
+<div class="">
+  Hola Tailwind!
 </div>
 `;
 
+const Training = () => {
+  const [code, setCode] = useState(defaultCode);
+  const [showEditor, setShowEditor] = useState(false);
+  const [lastTrainerText, setLastTrainerText] = useState(""); // View exercise button
+  const [showExplanation, setShowExplanation] = useState(false); // Request explanation
 
-const Game = () => {
+  //exercises and evaluate Answer
+  const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
+  const currentExercise = exercises[currentExerciseIndex];
 
-    const [code, setCode] = useState(defaultCode);
+  //Resize screens in width, preview screen and editor screen
+  const [previewWidth, setPreviewWidth] = useState(400); // start width
+
+  const resizerRef = useRef(null);
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPreviewWidth(e.clientX);
+    };
+
+    const stopResize = () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", stopResize);
+    };
+
+    const startResize = () => {
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", stopResize);
+    };
+
+    const resizer = resizerRef.current;
+    if (resizer) {
+      resizer.addEventListener("mousedown", startResize);
+    }
+
+    return () => {
+      if (resizer) resizer.removeEventListener("mousedown", startResize);
+    };
+  }, []);
+
+  const navigate = useNavigate();
+  const handleFinishLevel = () => {
+    navigateToNextPhase("Training", navigate);
+  };
 
   return (
     <div className="relative w-full min-h-screen">
@@ -28,17 +75,37 @@ const Game = () => {
         style={{ backgroundImage: `url(${TestFund})` }}
       ></div>
 
-      <Navigation />
-
-      <div className="grid items-center justify-center grid-cols-4 ">
-        <div className="relative h-[85%] mt-32 ">
+      <div className="flex h-screen gap-4 ">
+        <div
+          style={{ width: previewWidth }}
+          className="relative h-[85%] mt-32 "
+        >
           <Preview code={code} setCode={setCode} />
         </div>
 
-        <div className="relative grid min-h-screen grid-cols-1 col-span-3">
-          <h1 className="text-5xl font-bold text-black md:text-6xl drop-shadow-lg">
-            Lección 5: Game
-          </h1>
+        <div style={{ width: previewWidth }} className="absolute ">
+          <Navigation />
+        </div>
+
+        <div
+          ref={resizerRef}
+          className="relative w-2 bg-yellow-500 cursor-col-resize hover:bg-yellow-700"
+        ></div>
+
+        <div className="relative flex flex-col justify-end flex-1 min-h-screen">
+          <div className="bottom-0 w-full ">
+            {/* Scene sequence: coach's dialogue, exercise */}
+            {!showEditor && (
+              <DialogueBox
+                level={1}
+                currentPhase="Training"
+                onFinishDialog={(lastText) => {
+                  setShowEditor(true); // Show editor when dialog ends
+                  setLastTrainerText(lastText); // Last dialogue for the request explanation button
+                }}
+              />
+            )}
+          </div>
 
           <img
             src={RivalGoalkeeper}
@@ -46,17 +113,70 @@ const Game = () => {
             className="absolute object-contain w-80 top-1/4 right-1/2"
           />
 
-          <Clock />
+            <Clock className="absolute"/>
+   
 
-          {/* <TrainingPoint /> */}
+          {showEditor && (
+            <>
+              <div className="z-20 flex flex-col justify-center w-full h-full max-w-4xl gap-2 ">
+                <div className="flex flex-row w-full">
+                  <button
+                    className="max-w-[20rem] p-2 font-bold text-white bg-yellow-600 rounded  hover:bg-yellow-700"
+                    onClick={() => setShowExplanation(!showExplanation)}
+                  >
+                    {showExplanation
+                      ? "Ocultar explicación"
+                      : "Pedir explicaciones"}
+                  </button>
 
-          {/* <DialogueBox /> */}
+                  <button
+                    className="max-w-[20rem] p-2 font-bold text-white bg-sky-600 rounded  hover:bg-sky-700"
+                    onClick={() => {
+                      const isCorrect = evaluateAnswer(code, currentExercise);
 
-          <Editor code={code} setCode={setCode} />
+                      //Function to verify exercise as player response
+                      if (isCorrect) {
+                        alert(
+                          "¡Golazo! 🎯 ¡Has dominado la técnica! Sigue así y anotarás más goles. ¡Vamos por el siguiente ejercicio! ⚽"
+                        );
+
+                        if (currentExerciseIndex < exercises.length - 1) {
+                          // next exercise
+                          setCurrentExerciseIndex((prev) => prev + 1);
+                          setCode(defaultCode); // The code is reset when moving to the next exercise
+                        } else {
+                          // SWhen the exercises are finished, the coach congratulates you and you move on to the next phase of the game, which is the actual match.
+                          alert(
+                            "¡Bien! 🏆 ¡Has completado todos los ejercicios y dominado las técnicas! Ahora prepárate para el partido real. ¡A ganar! ⚽"
+                          );
+
+                          handleFinishLevel(); //directional button to other phases
+                        }
+                      } else {
+                        alert(
+                          "¡Casi! ❌ No te preocupes, sigue practicando. Recuerda, la técnica es clave. Vuelve a revisar y corrige el error. ¡La próxima vez será gol! ⚽"
+                        );
+                      }
+                    }}
+                  >
+                    Verificar respuesta
+                  </button>
+                </div>
+
+                {showExplanation && (
+                  <div className="w-full max-w-xl p-4 mt-2 text-black bg-white rounded-lg shadow-xl max-h-32">
+                    <strong>Entrenador dice:</strong>
+                    <p>{currentExercise.prompt}</p>
+                  </div>
+                )}
+                <Editor code={code} setCode={setCode} />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default Game;
+export default Training;
--- src/ui/Clock.jsx
+++ src/ui/Clock.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 const Clock = () => {
   return (
-    <div className="absolute top-0 grid grid-flow-col gap-5 text-center right-2 auto-cols-max">
+    <div className="absolute top-0 grid grid-flow-col gap-5 p-2 text-center text-white bg-black right-2 auto-cols-max">
 
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
         <span className="font-mono text-5xl countdown">
